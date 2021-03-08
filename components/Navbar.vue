0418d19666fb99a94b5cc1f0666b6f8244c3770f
Onkar Telange <Onkartelange2000@gmail.com>
2021-03-09 00:09:48 +0530

    added spacing to buttons

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -7,11 +7,11 @@
         </div>
       </router-link>
       <v-spacer />
-      <v-btn :to="'/sign-up'" text color="light">
+      <v-btn :to="'/sign-up'" text color="light" class="mx-2">
         <span>Sign Up</span>
-         <v-icon right>mdi-logout</v-icon> 
+         <v-icon right>mdi-logout</v-icon>
       </v-btn>
-      <v-btn :to="'/sign-in'" text color="light">
+      <v-btn :to="'/sign-in'" text color="light" class="mx-2">
         <span>Sign In</span>
         <v-icon right>mdi-login</v-icon>
       </v-btn>
