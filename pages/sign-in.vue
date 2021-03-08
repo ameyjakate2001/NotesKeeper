4d4b5541af526e9e3d443ae8d2657241d4b755b6
ameyjakate20001 <ameyjakate2001@gmail.com>
2021-03-08 14:08:11 -0800

    signIn added commit

---
+++ pages/sign-in.vue
@@ -0,0 +1,88 @@
+<template>
+  <v-app class="app">
+    <div>
+      <!-- <div class="form-width"> -->
+
+      <v-form
+        ref="form"
+        v-model="valid"
+        lazy-validation
+        class="myForm black pa-4"
+        @submit.prevent="submit"
+      >
+
+        <v-text-field
+          v-model="email"
+          :rules="emailRules"
+          label="E-mail"
+          color="purple darken-2"
+          class="mt-3"
+          required
+        />
+
+        <v-text-field
+          v-model="password"
+          :rules="passwordRule"
+          type="password"
+          label="Password"
+          color="purple darken-2"
+          class="mt-3 dark--text"
+          required
+        />
+
+        <v-btn class="mt-5 success" @click="submit">
+          LogIn
+        </v-btn>
+      </v-form>
+    </div>
+    <!-- </div> -->
+  </v-app>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    valid: false,
+    email: '',
+    password: '',
+    emailRules: [
+      v => !!v || 'E-mail is required',
+      v => /.+@.+\..+/.test(v) || 'E-mail must be valid'
+    ],
+    passwordRule: [
+      v => !!v || 'Password is required',
+      v => (v && v.length >= 5) || 'Password must have 5+ character',
+      v => /(?=.*[A-Z])/.test(v) || 'Must have one uppercase character',
+      v => /(?=.*\d)/.test(v) || 'Must have one number',
+      v => /([!@$%])/.test(v) || 'Must have one special character [!@#$%]'
+    ],
+    checkbox: false
+  }),
+
+  methods: {
+    submit () {
+      if (!this.$refs.form.validate()) {
+        alert('fill the correct info')
+      }
+      else {
+        alert(' ' + this.email + ' ' + this.password)
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.app{
+  background-color:whitesmoke;
+}
+  .myForm{
+    background-color: rgb(0, 0, 0);
+      border-radius: 10px;
+     max-width: 600px;
+     margin: 160px auto;
+      box-shadow: -1px 2px 5px 1px rgba(0,0,0,0.38);
+      -webkit-box-shadow: -1px 2px 5px 1px rgba(0,0,0,0.38);
+      -moz-box-shadow: -1px 2px 5px 1px rgba(0,0,0,0.38);
+  }
+</style>
