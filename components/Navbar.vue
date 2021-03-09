8d743532e6f83cdfd15e411471dac6dd6ec2f005
ameyjakate20001 <ameyjakate2001@gmail.com>
2021-03-08 23:17:22 -0800

    adding Navbar

---
+++ components/Navbar.vue
@@ -0,0 +1,20 @@
+<template>
+  <nav>
+    <v-app-bar flat>
+      <router-link :to="'/'">
+        <v-app-bar-title class="text-uppercase white--text">
+          <span class="font-weight-light">NotesKeeper</span>
+        </v-app-bar-title>
+      </router-link>
+      <v-spacer />
+      <v-btn :to="'/sign-up'" text color="light">
+        <span>Sign Up</span>
+        <!-- <v-icon right>mdi-logout</v-icon> -->
+      </v-btn>
+      <v-btn :to="'/sign-in'" text color="light">
+        <span>Sign In</span>
+        <v-icon right>mdi-login</v-icon>
+      </v-btn>
+    </v-app-bar>
+  </nav>
+</template>
