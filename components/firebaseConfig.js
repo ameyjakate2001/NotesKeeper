985ddde2c9891895ee883561e6231bfc38625175
ameyjakate20001 <ameyjakate2001@gmail.com>
2021-03-10 21:17:28 -0800

    configuration added

---
+++ components/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: 'AIzaSyCYjOWT87R2iao9bVVbMNYxjqYfoTcTr4Q',
+  authDomain: 'noteskeeper-9d923.firebaseapp.com',
+  projectId: 'noteskeeper-9d923',
+  storageBucket: 'noteskeeper-9d923.appspot.com',
+  messagingSenderId: '603684647189',
+  appId: '1:603684647189:web:78361eef1866a83cbeb91c',
+  measurementId: 'G-92PKTCJRLW'
+}
